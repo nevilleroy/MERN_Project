802be54a804a33e865f2d3ffdab12428cdd652f8
nevilleroy <52038531+nevilleroy@users.noreply.github.com>
2021-07-30 11:52:35 +0530

    Delete mern-eCommerce-app directory

--- mern-eCommerce-app/backend/utils/generate-token.js
+++
@@ -1,8 +0,0 @@
-import jwt from 'jsonwebtoken';
-
-// user id is using for generating token
-const generateToken = (id) => {
-  return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: '30d' });
-};
-
-export default generateToken;
